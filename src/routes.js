8a9cb0db4eb604eb4e746a787a634aa3e5815cf5
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-01-17 19:08:17 -0300

    I added the 'model' and inserted a record

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
 // const { Router } = require('express');
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.send('Paulo');
+routes.get('/', async (req, res) => {
+  await User.create({
+    name: 'Romi Rain',
+    email: 'contato@romirain.com',
+    password: '123'
+  }, (error, small) => {
+    if (error) return res.status(400).json({ error: "Usuário não cadastrado" });
+    
+    return res.status(200).json({ error: "Usuário cadastrado com sucesso" });
+  })
 });
 
 // module.exports = routes;
