307bfe775dea968edc449bd05036176fe8cf5e57
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-01-17 11:56:27 -0300

    BackendSite

---
+++ README.md
@@ -0,0 +1,11 @@
+Criando o arquivo 'package'
+### npm init
+
+Gerencia as requisições, rotas, URLs entre outras funcionalidades.
+### npm install express
+
+Instalar o module para reiniciar o servidor automaticamente sempre que houver uma mudança.
+### npm install -D nodemon
+
+Produz código JS válido para 'import' do ES6
+### npm install -D sucrase
---
+++ src/app.js
@@ -0,0 +1,17 @@
+// const express = require('express');
+import express from 'express';
+// const routes = require('./routes');
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.routes();
+  }
+  routes(){
+    this.app.use(routes)
+  }
+}
+
+// module.exports = new App().app;
+export default new App().app;
