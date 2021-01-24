2a1259b2a836d61d6a431ff817e788ff035d8eff
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-01-24 18:34:07 -0300

    I modularized the code, I created UserController file

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,23 @@
 // const { Router } = require('express');
 import { Router } from 'express';
 import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  await User.create({
-    name: 'Romi Rain',
-    email: 'contato@romirain.com',
-    password: '123'
-  }, (error, small) => {
-    if (error) return res.status(400).json({ error: "Usuário não cadastrado" });
+routes.post('/users', UserController.store);
+
+// routes.get('/', async (req, res) => {
+//   await User.create({
+//     name: 'Alison Tyler',
+//     email: 'contato@alisontyler.com',
+//     password: '123'
+//   }, (error, small) => {
+//     if (error) return res.status(400).json({ error: "Usuário não cadastrado" });
     
-    return res.status(200).json({ error: "Usuário cadastrado com sucesso" });
-  })
-});
+//     return res.status(200).json({ error: "Usuário cadastrado com sucesso" });
+//   })
+// });
 
 // module.exports = routes;
 export default routes;
